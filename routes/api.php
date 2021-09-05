d27ad12c15cc9513eb4ffa9e1a6aa88d794f2d11
NguyenTaiAnh <anhdv007@gmail.com>
2021-09-06 01:20:06 +0700

    Modified product details

--- routes/api.php
+++ routes/api.php
@@ -34,3 +34,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //    ]);
 //}) ;
 Route::apiResource('/products',\App\Http\Controllers\ProductController::class);
+Route::group(['prefix'=>'products'],function(){
+    Route::apiResource('/{product}/reviews',\App\Http\Controllers\ReviewController::class);
+});
